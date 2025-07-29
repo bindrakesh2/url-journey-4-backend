d9f9b633f8b9140a1a39e08c2ea031d830d3c027
bindrakesh2 <156573848+bindrakesh2@users.noreply.github.com>
2025-07-29 11:12:05 +0530

    Update build.sh
    removed admin rights

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-playwright install --with-deps chromium
+
+# FIX: Removed the --with-deps flag to prevent the permission error
+playwright install chromium
